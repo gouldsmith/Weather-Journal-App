9b1db925f188e437aa33a3775ee14be27fbcb472
David Gouldsmith <david.gouldsmith@willowtreeapps.com>
2020-04-05 16:35:55 -0400

    added server routes and chained promises

--- server.js
+++ server.js
@@ -2,6 +2,13 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require("express");
+const app = express();
+
+// Dependencies
+const bodyParser = require("body-parser");
+const cors = require("cors");
+app.use(cors());
 
 // Start up an instance of app
 
@@ -13,7 +20,36 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 
-
+const port = 3000;
 // Setup Server
+const server = app.listen(port, listening);
+
+function listening() {
+  console.log("Server is running");
+  console.log(`localhost: ${port}`);
+}
+
+// GET
+app.get("/all", sendData);
+
+function sendData(req, res) {
+  console.log(projectData);
+  res.send(projectData);
+}
+
+app.post("/add", addWeather);
+
+function addWeather(req, res) {
+  console.log(req.body);
+
+  newEntry = {
+    temp: req.body.temp,
+    feelings: req.body.feelings,
+    date: req.body.date,
+  };
+
+  projectData = newEntry;
+  res.send(projectData);
+}
--- website/app.js
+++ website/app.js
@@ -1,5 +1,66 @@
 /* Global Variables */
+let baseURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
+let apiKey = ",us&appid=9798e1521629e4757bb273c5e5aad438&units=imperial";
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+document.getElementById("generate").addEventListener("click", performAction);
+
+function performAction(e) {
+  const zipCode = document.getElementById("zip").value;
+  const feels = document.getElementById("feelings").value;
+
+  getWeather(baseURL, zipCode, apiKey, feels).then(function (data) {
+    console.log(
+      `It's ${data.main.feels_like} degrees out and I'm ${feels} on ${newDate}`
+    );
+    postData("/add", {
+      temp: data.main.feels_like,
+      feelings: feels,
+      date: newDate,
+    })
+    .then(updateUI())
+  });
+}
+
+const getWeather = async (baseURL, zipCode, apiKey) => {
+  const res = await fetch(baseURL + zipCode + apiKey);
+  try {
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+// POST Request
+const postData = async (url = "", data = {}) => {
+  const res = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  console.log(data);
+};
+
+// Update UI
+const updateUI = async (url = "/all") => {
+  const req = await fetch(url);
+  try {
+    const allData = await req.json();
+    console.log(allData)
+    // return allData;
+    // add data to UI
+    document.getElementById('temp').innerHTML = allData.temp;
+    document.getElementById('date').innerHTML = allData.date;
+    document.getElementById('content').innerHTML = allData.feelings;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
